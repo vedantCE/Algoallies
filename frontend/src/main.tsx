71f1ea51c0230f1b57fc417fda0f2c316db27194
VEDANT BHATT <vedantbhattce28@gmail.com>
2025-12-18 23:41:30 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
+import { Analytics } from "@vercel/analytics/react";
+createRoot(document.getElementById("root")!).render(
+ <>
+    <App />
+    <Analytics />
+  </>
+);
